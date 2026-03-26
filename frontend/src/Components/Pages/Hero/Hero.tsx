b75431f7703733c8eb2f8faef6bae0437e6f9a17
botoli <standrey2007@gmail.com>
2026-03-26 17:57:15 +0300

    commit

--- frontend/src/Components/Pages/Hero/Hero.tsx
+++ frontend/src/Components/Pages/Hero/Hero.tsx
@@ -16,7 +16,7 @@ function Hero() {
   const [categoryFilter, setCategoryFilter] = useState<string>(ALL_CATEGORIES);
 
   useEffect(() => {
-    fetch("http://95.165.29.213:8081/products")
+    fetch("https://cluster.novextask.ru/products")
       .then((response) => {
         if (!response.ok) throw new Error("Ошибка сети");
         return response.json() as Promise<ProductsResponse>;
